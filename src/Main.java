eccd54fe67a4f6cb10ef69211ba723bd69f583b9
abdullayevsema <info.pelikantobacco@gmail.com>
2022-12-08 20:23:27 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+      int x=782;
+      int x1=7;
+      int x2=8;
+      int x3=2;
+
+        System.out.println("int="+x1);
+        System.out.println("int="+x2);
+        System.out.println("int="+x3);
+
+
+
+    }
+}
